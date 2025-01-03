4dce1d28b8441f4a8858541f0e60fcf88b26d2d9
Gopi Krishna Pasivedala <gkpasivedala@compreo.ai>
2025-01-03 18:14:32 +0530

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using BankingApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingApplication.Controllers
+{
+    [Route("accounts")]
+    public class AccountController : Controller
+    {
+        [HttpGet("all")]
+        public IActionResult Index()
+        {
+            List<AccountDetails> details = new List<AccountDetails>();
+            return View(details);
+        }
+        [HttpGet("create-account")]
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
+        [HttpGet("edit-account/{id}")]
+        public IActionResult EditAccount()
+        {
+            return View();
+        }
+        [HttpDelete("delete-account")]
+        public IActionResult DeleteAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/AccountDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingApplication.Models
+{
+    public class AccountDetails
+    {
+        public int Id { get; set; }
+        public required string AccountName { get; set; }
+        public decimal AccountBalance { get; set; }
+    }
+}
